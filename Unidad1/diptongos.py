0463da3a6c187de9571193ef69cec337486a0339
Hector Rodriguez <hector@empleosti.com.mx>
2019-01-23 09:52:48 -0600

    Terminado

---
+++ Unidad1/diptongos.py
@@ -0,0 +1,22 @@
+
+vocales = ['a', 'e', 'i', 'o', 'u']
+
+with open('parrafo.txt', encoding="utf-8") as file:
+    lineNumber = 1
+    for line in file:
+        words = line.split(" ")
+        for word in words:
+            if word[len(word) - 1] == '\n':
+                word = word[0:len(word) - 2]
+            vocalAntes = ''
+            for char in word:
+                if char in vocales:
+                    if not vocalAntes == '':
+                        print()
+                        print('Palabra:', word)
+                        print('Diptongo: {}{}'.format(vocalAntes, char))
+                        print('En la linea', lineNumber)
+                    vocalAntes = char
+                else:
+                    vocalAntes = ''
+        lineNumber += 1
